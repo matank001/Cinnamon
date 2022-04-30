d3d66be3da4ca1ee70af549ae065596e8d8beefa
dangutstadt <dangutstadt1998@gmail.com>
2022-04-30 19:30:44 +0300

    now the password will be passed by the python remote_init script

--- create_profile.sh
+++ create_profile.sh
@@ -1,13 +1,13 @@
 mkdir profile
 
 # be sure that it has those cause if not we are in a bad prob...
-sudo apt install make
-sudo apt install gcc
-sudo apt install dwarfdump
+echo $1 | sudo -S apt install make
+echo $1 | sudo -S apt install gcc
+echo $1 | sudo -S apt install dwarfdump
 
 echo 'Done making...'
 cd volatility/tools/linux/ && make
 cd ../../../
 cd profile
 echo 'Done making...'
-echo 1 | sudo -S zip $(lsb_release -i -s)_$(uname -r)_profile.zip ../volatility/tools/linux/module.dwarf /boot/System.map-$(uname -r)
+echo $1 | sudo -S zip $(lsb_release -i -s)_$(uname -r)_profile.zip ../volatility/tools/linux/module.dwarf /boot/System.map-$(uname -r)
